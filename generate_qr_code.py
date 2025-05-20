b8b02f563e857876ec563e4f6c037999c132ba38
ShiKikie <toon1236@gmail.com>
2025-05-20 16:00:51 +0700

    genrate advance

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -2,8 +2,8 @@ import qrcode
 from datetime import datetime 
 
 now = datetime.now()
-current_time = now.strftime("%m-%d-%Y-%H.%M.%S")
+current_time = now.strftime("%d-%m-%Y-%H.%M.%S")  ##Ext 20-05-2025-15.38.50
 
-img = qrcode.make('Some data here')
-type(img)  # qrcode.image.pil.PilImage
+img = qrcode.make('Some data here')  #input data here
+type(img)  
 img.save("./qr-code/"+current_time+".png")
---
+++ generate_qr_code_color.py
@@ -0,0 +1,20 @@
+import qrcode
+from datetime import datetime 
+
+now = datetime.now()
+current_time = now.strftime("%d-%m-%Y-%H.%M.%S")  ##Ext 20-05-2025-15.38.50
+
+qr = qrcode.QRCode(
+    version=6,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=20,
+    border=2,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="blue", back_color="white")
+# img = qr.make_image(back_color=(255, 195, 235), fill_color=(55, 95, 35))
+
+type(img)  
+img.save("./qr-code/"+current_time+".png")
